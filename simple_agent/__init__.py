a169ebead71859231ad9bce9999790c935d26b4e
Bohdan Agarkov <sidi0us.mail@gmail.com>
2024-04-14 17:56:54 +0200

    Added launch scripts.

---
+++ simple_agent/__init__.py
@@ -0,0 +1,20 @@
+"""Launch helpers for Poetry."""
+
+import subprocess
+import sys
+
+
+def main():
+    """Launches app normally, not in development mode."""
+    try:
+        subprocess.run(["chainlit", "run", "./simple_agent/app.py"], check=True)
+    except KeyboardInterrupt:
+        sys.exit(0)
+
+
+def dev():
+    """Starts development server which updates upon saving."""
+    try:
+        subprocess.run(["chainlit", "run", "./simple_agent/app.py", "-w"], check=True)
+    except KeyboardInterrupt:
+        sys.exit(0)
